068e14afcf5e6da6fc77a0935c0c4d3b89735a01
pycckuu <is.markelov@gmail.com>
2017-01-15 23:31:02 -0500

    updated test version of ODE

--- test.py
+++ test.py
@@ -1,54 +1,55 @@
-    # k_1 = dt*sediment_rates(C0, dt);
-    # k_2 = dt*sediment_rates(C0+0.5*k_1, dt);
-    # k_3 = dt*sediment_rates(C0+0.5*k_2, dt);
-    # k_4 = dt*sediment_rates(C0+k_3, dt);
-    # C_new = C0 + (k_1+2*k_2+2*k_3+k_4)/6;
+# k_1 = dt*sediment_rates(C0, dt);
+# k_2 = dt*sediment_rates(C0+0.5*k_1, dt);
+# k_3 = dt*sediment_rates(C0+0.5*k_2, dt);
+# k_4 = dt*sediment_rates(C0+k_3, dt);
+# C_new = C0 + (k_1+2*k_2+2*k_3+k_4)/6;
 
-    import numpy as np
+import numpy as np
 
-    dt = 0.1
+dt = 0.1
 
-    coef = {'k': 1}
-    rates = {'R1': 'k*OM*O2'}
-    dcdt = {'O2': '-4 * R1', 'OM': '-R1'}
-    C0 = {'OM': np.array([0, 0.1, 0.2, 0.3]), 'O2': np.array([0, 0.1, 0.2, 0.3])}
-
-
-    def k_loop(conc):
-        rates_num = {}
-
-        for k in rates:
-            rates_num[k] = eval(rates[k], {**coef, **conc})
-
-        dcdt_num = {}
-
-        for k in dcdt:
-            dcdt_num[k] = eval(dcdt[k], rates_num)
-
-        Kn = {}
-
-        for k in C0:
-            Kn[k] = dt * dcdt_num[k]
-        return Kn
+coef = {'k': 1}
+rates = {'R1': 'k*y1*y2'}
+dcdt = {'y1': '-4 * R1', 'y2': '-R1'}
+C0 = {'y2': np.array([0, 0.1, 0.2, 0.3]), 'y1': np.array([0, 0.1, 0.2, 0.3])}
 
 
-    def sum_k(A, B, b):
-        C = {}
-        for k in A:
-            C[k] = A[k] + b * B[k] * dt
-        return C
+def k_loop(conc):
+    rates_num = {}
+
+    for k in rates:
+        rates_num[k] = eval(rates[k], {**coef, **conc})
+
+    dcdt_num = {}
+
+    for k in dcdt:
+        dcdt_num[k] = eval(dcdt[k], rates_num)
+
+    Kn = {}
+
+    for k in C0:
+        Kn[k] = dt * dcdt_num[k]
+    return Kn
 
 
-    def result(C_0, k_1, k_2, k_3, k_4):
-        C_new = {}
-        for k in C_0:
-            C_new[k] = C_0[k] + (k_1[k] + 2 * k_2[k] + 2 * k_3[k] + k_4[k]) / 6
-        return C_new
+def sum_k(A, B, b):
+    C = {}
+    for k in A:
+        C[k] = A[k] + b * B[k] * dt
+    return C
 
 
-    k1 = k_loop(C0)
-    k2 = k_loop(sum_k(C0, k1, 0.5))
-    k3 = k_loop(sum_k(C0, k2, 0.5))
-    k4 = k_loop(sum_k(C0, k3, 1))
+def result(C_0, k_1, k_2, k_3, k_4):
+    C_new = {}
+    for k in C_0:
+        C_new[k] = C_0[k] + (k_1[k] + 2 * k_2[k] + 2 * k_3[k] + k_4[k]) / 6
+    return C_new
 
-    C_new = result(C0, k1, k2, k3, k4)
+
+k1 = k_loop(C0)
+k2 = k_loop(sum_k(C0, k1, 0.5))
+k3 = k_loop(sum_k(C0, k2, 0.5))
+k4 = k_loop(sum_k(C0, k3, 1))
+
+C_new = result(C0, k1, k2, k3, k4)
+print(C_new)
