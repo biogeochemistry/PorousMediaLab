3cf383b3e48d489da63617d3e32f3b1f44eb8010
pycckuu <is.markelov@gmail.com>
2017-01-15 22:29:31 -0500

    test of Python eval()

--- test.py
+++ test.py
@@ -1,34 +1,54 @@
-import numpy as np
+    # k_1 = dt*sediment_rates(C0, dt);
+    # k_2 = dt*sediment_rates(C0+0.5*k_1, dt);
+    # k_3 = dt*sediment_rates(C0+0.5*k_2, dt);
+    # k_4 = dt*sediment_rates(C0+k_3, dt);
+    # C_new = C0 + (k_1+2*k_2+2*k_3+k_4)/6;
 
-dt = 0.1
+    import numpy as np
 
-conc = {'OM': np.array([0, 1, 2, 3]), 'O2': np.array([0, 1, 2, 3])}
-coef = {'k': 1}
-rates = {'R1':'k*OM*O2'}
-dcdt = {'O2':'-4 * R1', 'OM': '-R1'}
+    dt = 0.1
 
-rates_num = {}
-
-for k in rates:
-    rates_num[k] = eval(rates[k], {**coef, **conc})
-
-dcdt_num={}
-for k in dcdt:
-    dcdt_num[k] = eval(dcdt[k], rates_num)
-
-# k1 = {}
-# k2 = {}
-# k3 = {}
-# k4 = {}
-# k5 = {}
-
-# for k in conc:
-#     k1[k] = dt*dcdt[k]
+    coef = {'k': 1}
+    rates = {'R1': 'k*OM*O2'}
+    dcdt = {'O2': '-4 * R1', 'OM': '-R1'}
+    C0 = {'OM': np.array([0, 0.1, 0.2, 0.3]), 'O2': np.array([0, 0.1, 0.2, 0.3])}
 
 
+    def k_loop(conc):
+        rates_num = {}
 
-# for k in conc:
+        for k in rates:
+            rates_num[k] = eval(rates[k], {**coef, **conc})
 
-# print(rates_num)
-# a = eval('-R1', rates_num)
-print(dcdt_num)
+        dcdt_num = {}
+
+        for k in dcdt:
+            dcdt_num[k] = eval(dcdt[k], rates_num)
+
+        Kn = {}
+
+        for k in C0:
+            Kn[k] = dt * dcdt_num[k]
+        return Kn
+
+
+    def sum_k(A, B, b):
+        C = {}
+        for k in A:
+            C[k] = A[k] + b * B[k] * dt
+        return C
+
+
+    def result(C_0, k_1, k_2, k_3, k_4):
+        C_new = {}
+        for k in C_0:
+            C_new[k] = C_0[k] + (k_1[k] + 2 * k_2[k] + 2 * k_3[k] + k_4[k]) / 6
+        return C_new
+
+
+    k1 = k_loop(C0)
+    k2 = k_loop(sum_k(C0, k1, 0.5))
+    k3 = k_loop(sum_k(C0, k2, 0.5))
+    k4 = k_loop(sum_k(C0, k3, 1))
+
+    C_new = result(C0, k1, k2, k3, k4)
