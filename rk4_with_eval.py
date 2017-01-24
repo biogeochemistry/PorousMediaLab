957e8cbd12af2ba62e8a0c7699d0db8b0288dfd1
pycckuu <is.markelov@gmail.com>
2017-01-23 22:18:09 -0500

    rk4 for chemical reactions

--- rk4_with_eval.py
+++ rk4_with_eval.py
@@ -6,51 +6,100 @@
 # C_new = C0 + (k_1+2*k_2+2*k_3+k_4)/6
 
 import numpy as np
-
-dt = 0.1
-
-coef = {'k': 1}
-rates = {'R1': 'k*y1*y2'}
-dcdt = {'y1': '-4 * R1', 'y2': '-R1'}
-C0 = {'y2': np.array([0, 0.1, 0.2, 0.3]), 'y1': np.array([0, 0.1, 0.2, 0.3])}
+import numexpr as ne
+import matplotlib.pyplot as plt
 
 
-def k_loop(conc):
-    rates_num = {}
+def fun():
+    dt = 1e-4
 
-    for k in rates:
-        rates_num[k] = eval(rates[k], {**coef, **conc})
+    coef = {'k': 1}
+    rates = {'R1': 'k*y1'}
+    dcdt = {'y1': '-R1', 'y2': '-R1'}
+    C = {'y1': np.random.rand(400), 'y2': np.random.rand(400)}
 
-    dcdt_num = {}
+    def k_loop(conc):
+        rates_num = {}
 
-    for k in dcdt:
-        dcdt_num[k] = eval(dcdt[k], rates_num)
+        for k in rates:
+            rates_num[k] = ne.evaluate(rates[k], {**coef, **conc})
 
-    Kn = {}
+        dcdt_num = {}
 
-    for k in C0:
-        Kn[k] = dt * dcdt_num[k]
-    return Kn
+        for k in dcdt:
+            dcdt_num[k] = ne.evaluate(dcdt[k], rates_num)
+
+        Kn = {}
+
+        for k in C:
+            Kn[k] = dt * dcdt_num[k]
+        return Kn
+
+    def sum_k(A, B, b):
+        C = {}
+        for k in A:
+            C[k] = A[k] + b * B[k] * dt
+        return C
+
+    def result(C_0, k_1, k_2, k_3, k_4):
+        C_new = {}
+        for k in C_0:
+            C_new[k] = C_0[k] + (k_1[k] + 2 * k_2[k] + 2 * k_3[k] + k_4[k]) / 6
+        return C_new
+
+    # k1 = k_loop(C)
+    # k2 = k_loop(sum_k(C, k1, 0.5))
+    # k3 = k_loop(sum_k(C, k2, 0.5))
+    # k4 = k_loop(sum_k(C, k3, 1))
+
+    # res[0] = C[0]
+    # C = result(C, k1, k2, k3, k4)
+    # res = np.zeros(10000)
+    # for i in range(10000):
+    # res[i] = C['y1'][0]
+    k1 = k_loop(C)
+    k2 = k_loop(sum_k(C, k1, 0.5))
+    k3 = k_loop(sum_k(C, k2, 0.5))
+    k4 = k_loop(sum_k(C, k3, 1))
+    C = result(C, k1, k2, k3, k4)
+    # print(C_new)
+
+    # return res
+
+    # x = np.arange(0, 1, 0.1)
+    # plt.plot(C_new['y1'])
+    # plt.plot(np.exp(-x))
+    # plt.show()
 
 
-def sum_k(A, B, b):
-    C = {}
-    for k in A:
-        C[k] = A[k] + b * B[k] * dt
-    return C
+def fun_2():
+    # gives very high error
+    dt = 1e-4
 
+    c = np.array([1])
+    y = np.array([np.random.rand(400), np.random.rand(400)])
+    R = np.array(['c[0]*y[0]*y[1]'])
+    dcdt = np.array(['-4 * R[0]', '-R[0]'])
 
-def result(C_0, k_1, k_2, k_3, k_4):
-    C_new = {}
-    for k in C_0:
-        C_new[k] = C_0[k] + (k_1[k] + 2 * k_2[k] + 2 * k_3[k] + k_4[k]) / 6
-    return C_new
+    def k_loop(y):
+        rates_num = np.array([eval(k, {'c': c, 'y': y}) for k in R])
+        dcdt_num = np.array([eval(k, {'R': rates_num}) for k in dcdt])
+        return dt * dcdt_num
 
+    def sum_k(A, B, b):
+        return A + b * B * dt
 
-k1 = k_loop(C0)
-k2 = k_loop(sum_k(C0, k1, 0.5))
-k3 = k_loop(sum_k(C0, k2, 0.5))
-k4 = k_loop(sum_k(C0, k3, 1))
+    def result(y_0, k_1, k_2, k_3, k_4):
+        return y_0 + (k_1 + 2 * k_2 + 2 * k_3 + k_4) / 6
 
-C_new = result(C0, k1, k2, k3, k4)
-print(C_new)
+    # res = np.zeros(10000)
+    # for i in range(10000):
+        # res[i] = y[0][0]
+    k1 = k_loop(y)
+    k2 = k_loop(sum_k(y, k1, 0.5))
+    k3 = k_loop(sum_k(y, k2, 0.5))
+    k4 = k_loop(sum_k(y, k3, 1))
+    y = result(y, k1, k2, k3, k4)
+    # print(C_new)
+
+    return y
--- sediment_class.py
+++ sediment_class.py
@@ -1,16 +1,16 @@
 import numpy as np
 import scipy as sp
 from scipy.sparse import spdiags
+from scipy.sparse import linalg
 from scipy import special
 import matplotlib.pyplot as plt
-# from scikits import umfpack
+import numexpr as ne
 
 
 class Sediment:
-    """Sediment class"""
+    """Sediment module solves Advection-Diffusion-Reaction Equation"""
 
     def __init__(self, L, dx, tend, dt, phi, w):
-        super(Sediment, self).__init__()
         self.L = L
         self.dx = dx
         self.tend = tend
@@ -18,127 +18,198 @@ class Sediment:
         self.phi = phi
         self.w = w
         self.species = {}
+        self.profiles = {}
         self.dcdt = {}
         self.rates = {}
+        self.estimated_rates = {}
         self.constants = {}
         self.x = np.linspace(0, L, L / dx + 1)
         self.time = np.linspace(0, tend, tend / dt + 1)
         self.N = self.x.size
 
-    def add_species(self, name, D, init_C, BC_top, is_solute):
-        self.species[name] = {}
-        self.species[name]['solute?'] = is_solute
-        self.species[name]['bc_top'] = BC_top
-        self.species[name]['fx_bottom'] = 0
-        self.species[name]['D'] = D
-        self.species[name]['res'] = np.zeros((self.N, self.time.size))
-        self.species[name]['res'][:, 0] = (init_C * np.ones((self.N)))
-        self.create_AL_AR(name)
-        self.update_bc(name, 0)
-        self.dcdt[name] = 0
+    def add_species(self, element, D, init_C, BC_top, is_solute):
+        self.species[element] = {}
+        self.species[element]['solute?'] = is_solute
+        self.species[element]['bc_top'] = BC_top
+        self.species[element]['fx_bottom'] = 0
+        self.species[element]['D'] = D
+        self.species[element]['concentration'] = np.zeros((self.N, self.time.size))
+        self.species[element]['concentration'][:, 0] = (init_C * np.ones((self.N)))
+        self.profiles[element] = self.species[element]['concentration'][:, 0]
+        self.create_AL_AR(element)
+        self.update_matrices_due_to_bc(element, 0)
+        self.dcdt[element] = 0
 
-    def add_solute_species(self, name, D, init_C, BC_top):
-        self.add_species(name, D, init_C, BC_top, True)
+    def add_solute_species(self, element, D, init_C, BC_top):
+        self.add_species(element, D, init_C, BC_top, True)
 
-    def add_solid_species(self, name, D, init_C, BC_top):
-        self.add_species(name, D, init_C, BC_top, False)
+    def add_solid_species(self, element, D, init_C, BC_top):
+        self.add_species(element, D, init_C, BC_top, False)
 
-    def template_AL_AR(self, name, kappa):
+    def template_AL_AR(self, element, kappa):
         e1 = np.ones((self.N, 1))
-        s = kappa * self.species[name]['D'] * self.dt / self.dx / self.dx  #
+        s = kappa * self.species[element]['D'] * self.dt / self.dx / self.dx  #
         q = kappa * self.w * self.dt / self.dx
-        self.species[name]['AL'] = spdiags(np.concatenate((e1 * (-s / 2 - q / 4), e1 * (kappa + s), e1 * (-s / 2 + q / 4)), axis=1).T, [-1, 0, 1], self.N, self.N, format='csc')  # .toarray()
-        self.species[name]['AR'] = spdiags(np.concatenate((e1 * (s / 2 + q / 4), e1 * (kappa - s), e1 * (s / 2 - q / 4)), axis=1).T, [-1, 0, 1], self.N, self.N, format='csc')  # .toarray()
-        self.species[name]['AL'][-1, -1] = kappa + s
-        self.species[name]['AL'][-1, -2] = -s
-        self.species[name]['AR'][-1, -1] = kappa - s
-        self.species[name]['AR'][-1, -2] = s
+        self.species[element]['AL'] = spdiags(np.concatenate((e1 * (-s / 2 - q / 4), e1 * (kappa + s), e1 * (-s / 2 + q / 4)),
+                                                             axis=1).T, [-1, 0, 1], self.N, self.N, format='csc')  # .toarray()
+        self.species[element]['AR'] = spdiags(np.concatenate((e1 * (s / 2 + q / 4), e1 * (kappa - s), e1 * (s / 2 - q / 4)),
+                                                             axis=1).T, [-1, 0, 1], self.N, self.N, format='csc')  # .toarray()
+        self.species[element]['AL'][-1, -1] = kappa + s
+        self.species[element]['AL'][-1, -2] = -s
+        self.species[element]['AR'][-1, -1] = kappa - s
+        self.species[element]['AR'][-1, -2] = s
 
-        if self.species[name]['solute?']:
-            self.species[name]['AL'][0, 0] = kappa
-            self.species[name]['AL'][0, 1] = 0
-            self.species[name]['AR'][0, 0] = kappa
-            self.species[name]['AR'][0, 1] = 0
+        if self.species[element]['solute?']:
+            self.species[element]['AL'][0, 0] = kappa
+            self.species[element]['AL'][0, 1] = 0
+            self.species[element]['AR'][0, 0] = kappa
+            self.species[element]['AR'][0, 1] = 0
         else:
-            self.species[name]['AL'][0, 0] = kappa + s
-            self.species[name]['AL'][0, 1] = -s
-            self.species[name]['AR'][0, 0] = kappa - s
-            self.species[name]['AR'][0, 1] = s
+            self.species[element]['AL'][0, 0] = kappa + s
+            self.species[element]['AL'][0, 1] = -s
+            self.species[element]['AR'][0, 0] = kappa - s
+            self.species[element]['AR'][0, 1] = s
 
-    def create_AL_AR(self, name):
+    def create_AL_AR(self, element):
         # create_AL_AR: creates AL and AR matrices
-        if self.species[name]['solute?']:
+        if self.species[element]['solute?']:
             kappa = self.phi
         else:
             kappa = 1 - self.phi
-        self.template_AL_AR(name, kappa)
+        self.template_AL_AR(element, kappa)
 
-    def update_bc(self, name, i):
-        if self.species[name]['solute?']:
-            self.species[name]['res'][0, i] = self.species[name]['bc_top']
-            self.species[name]['B'] = self.species[name]['AR'].dot(self.species[name]['res'][:, i])
+    def new_boundary_condition(self, element, bc):
+        self.species[element]['bc_top'] = bc
+
+    def update_matrices_due_to_bc(self, element, i):
+        if self.species[element]['solute?']:
+            self.species[element]['concentration'][0, i] = self.species[element]['bc_top']
+            self.species[element]['B'] = self.species[element]['AR'].dot(self.species[element]['concentration'][:, i])
             # Lower BC is F = w*phi*C
-            s = self.phi * self.species[name]['D'] * self.dt / self.dx / self.dx
-            self.species[name]['B'][-1] = self.species[name]['B'][-1] - 2 * self.species[name]['res'][-1, i] * self.phi * self.w * s * self.dx / self.phi / self.species[name]['D']
+            # s = self.phi * self.species[element]['D'] * self.dt / self.dx / self.dx
+            # self.species[element]['B'][-1] = self.species[element]['B'][-1] - 2 * self.species[element]['concentration'][-1, i] * \
+            # self.phi * self.w * s * self.dx / self.phi / self.species[element]['D']
         else:
-            self.species[name]['B'] = self.species[name]['AR'].dot(self.species[name]['res'][:, i])
-            s = (1 - self.phi) * self.species[name]['D'] * self.dt / self.dx / self.dx
-            self.species[name]['B'][0] = self.species[name]['B'][0] + 2 * self.species[name]['bc_top'] * s * self.dx / (1 - self.phi) / self.species[name]['D']
+            self.species[element]['B'] = self.species[element]['AR'].dot(self.species[element]['concentration'][:, i])
+            s = (1 - self.phi) * self.species[element]['D'] * self.dt / self.dx / self.dx
+            self.species[element]['B'][0] = self.species[element]['B'][0] + 2 * self.species[element]['bc_top'] * s * self.dx / (1 - self.phi) / self.species[element]['D']
             # Lower BC is F = w*(1-phi)*C
-            s = (1 - self.phi) * self.species[name]['D'] * self.dt / self.dx / self.dx
-            self.species[name]['B'][-1] = self.species[name]['B'][-1] - 2 * self.species[name]['res'][-1, i] * (1 - self.phi) * self.w * s * self.dx / (1 - self.phi) / self.species[name]['D']
+            s = (1 - self.phi) * self.species[element]['D'] * self.dt / self.dx / self.dx
+            self.species[element]['B'][-1] = self.species[element]['B'][-1] - 2 * self.species[element]['concentration'][-1, i] * \
+                (1 - self.phi) * self.w * s * self.dx / (1 - self.phi) / self.species[element]['D']
 
     def solve(self):
         for i in np.arange(1, len(self.time)):
-            # self.reactions_integrate(i)
-            self.transport_integrate(i)
+            self.integrate_one_timestep(i)
+
+    def integrate_one_timestep(self, i):
+        self.transport_integrate(i)
+        self.reactions_integrate(i)
 
     def transport_integrate(self, i):
-        for name in self.species:
-            self.species[name]['res'][:, i] = sp.sparse.linalg.spsolve(self.species[name]['AL'], self.species[name]['B'], use_umfpack=True)
-            self.update_bc(name, i)
+        for element in self.species:
+            self.species[element]['concentration'][:, i] = linalg.spsolve(self.species[element]['AL'], self.species[element]['B'], use_umfpack=True)
+            self.update_matrices_due_to_bc(element, i)
+            self.profiles[element] = self.species[element]['concentration'][:, i]
 
-    # def reactions_integrate(self, name, i):
-    #     for name in self.species:
-    #         k_1 = dt * sediment.rates(C0, dt)
-    #         k_2 = dt * sediment.rates(C0 + 1 / 4 * k_1, dt)
-    #         k_3 = dt * sediment.rates(C0 + 1 / 8 * k_1 + 1 / 8 * k_2, dt)
-    #         k_4 = dt * sediment.rates(C0 - 1 / 2 * k_2 + k_3, dt)
-    #         k_5 = dt * sediment.rates(C0 + 3 / 16 * k_1 + 9 / 16 * k_4, dt)
-    #         k_6 = dt * sediment.rates(C0 - 3 / 7 * k_1 + 2 / 7 * k_2 + 12 / 7 * k_3 - 12 / 7 * k_4 + 8 / 7 * k_5, dt)
-    #         C0 = C0 + (7 * k_1 + 32 * k_3 + 12 * k_4 + 32 * k_5 + 7 * k_6) / 90
+    def reactions_integrate(self, i):
+        C_new, rates = runge_kutta_integrate(self.profiles, self.dcdt, self.rates, self.constants, self.dt)
+
+        for element in C_new:
+            self.species[element]['concentration'][:, i] = C_new[element] * (C_new[element] > 0)
+            self.update_matrices_due_to_bc(element, i)
+            self.profiles[element] = self.species[element]['concentration'][:, i]
+            self.estimated_rates[element] = rates[element]
+
+
+def runge_kutta_integrate(C0, dcdt, rates, coef, dt):
+    """Integrates the reactions according to 4th Order Runge-Kutta method
+     k_1 = dt*dcdt(C0, dt)
+     k_2 = dt*dcdt(C0+0.5*k_1, dt)
+     k_3 = dt*dcdt(C0+0.5*k_2, dt)
+     k_4 = dt*dcdt(C0+k_3, dt)
+     C_new = C0 + (k_1+2*k_2+2*k_3+k_4)/6
+    """
+
+    def k_loop(conc):
+        rates_num = {}
+
+        for element in rates:
+            rates_num[element] = ne.evaluate(rates[element], {**coef, **conc})
+
+        dcdt_num = {}
+
+        for element in dcdt:
+            dcdt_num[element] = ne.evaluate(dcdt[element], rates_num)
+
+        Kn = {}
+
+        for element in conc:
+            Kn[element] = dt * dcdt_num[element]
+        return Kn
+
+    def sum_k(A, B, b):
+        C_new = {}
+        for k in A:
+            C_new[k] = A[k] + b * B[k] * dt
+        return C_new
+
+    def rk4(C_0):
+        k1 = k_loop(C_0)
+        k2 = k_loop(sum_k(C_0, k1, 0.5))
+        k3 = k_loop(sum_k(C_0, k2, 0.5))
+        k4 = k_loop(sum_k(C_0, k3, 1))
+        C_new = {}
+        num_rates = {}
+        for element in C_0:
+            num_rates[element] = (k1[element] + 2 * k2[element] + 2 * k3[element] + k4[element]) / 6 / dt
+            C_new[element] = C_0[element] + num_rates[element] * dt
+        return C_new, num_rates
+
+    return rk4(C0)
+
 
 if __name__ == '__main__':
-    D = 40
+    D = 269
     w = 1
-    t = 1
+    t = 2
     dx = 0.1
     L = 10
     phi = 0.9
-    dt = 0.001
+    dt = 0.0001
+    rho =
 
-    # dcdt(:,1)  = -0.25 * R8  - 2 * R9  - R1 * F
+    time = np.linspace(0, t, t / dt + 1)
 
     sediment = Sediment(L, dx, t, dt, phi, w)
-    sediment.add_solute_species('O2', D, 0.0, 1)
-    sediment.add_solid_species('OM', 0.1, 0.0, 1)
-    sediment.constants['k'] = 1
-    sediment.rates['R1'] = '-k * O2 * OM'
+    sediment.add_solute_species('O2', D, 0.0, 0.15)
+    sediment.add_solid_species('OM', 0.1, 1., 1)
+    sediment.constants['k'] = 0.1
+    sediment.constants['Km_O2'] = 0.0123
+    sediment.rates['R1'] = 'k * OM * O2/(O2+Km_O2)'
     sediment.dcdt['O2'] = '-4 * R1'
     sediment.dcdt['OM'] = '-R1'
-    sediment.solve()
 
-    x = np.linspace(0, L, L / dx + 1)
-    sol = 1 / 2 * (special.erfc((x - w * t) / 2 / np.sqrt(D * t)) + np.exp(w * x / D) * special.erfc((x + w * t) / 2 / np.sqrt(D * t)))
+    for i in np.arange(1, len(time)):
+        bc = 0.15 + 0.1 * np.sin(time[i] * 2 * 3.14)
+        sediment.new_boundary_condition('O2', bc)
+        sediment.integrate_one_timestep(i)
+
+    # sediment.solve()
+
+    # x = np.linspace(0, L, L / dx + 1)
+    # sol = 1 / 2 * (special.erfc((x - w * t) / 2 / np.sqrt(D * t)) + np.exp(w * x / D) * special.erfc((x + w * t) / 2 / np.sqrt(D * t)))
 
     # Bulk concentrations
+    # TODO how to convert bulk to mol/mg and conversion factor F?
     plt.figure()
     plt.title('Bulk concentrations')
-    plt.plot(sediment.x, phi * sediment.species['O2']['res'][:, -1], 'k')
-    plt.plot(sediment.x, (1 - phi) * sediment.species['OM']['res'][:, -1], 'b')
+    plt.plot(sediment.x, phi * sediment.species['O2']['concentration'][:, -1], 'k')
+    plt.plot(sediment.x, (1 - phi) * sediment.species['OM']['concentration'][:, -1], 'b')
+    plt.show()
 
     plt.figure()
     plt.title('Aq and solid concentrations')
-    plt.plot(sediment.x, sediment.species['O2']['res'][:, -1], 'k')
-    plt.plot(sediment.x, sediment.species['OM']['res'][:, -1], 'b')
+    plt.plot(sediment.x, sediment.species['O2']['concentration'][:, -1], 'k')
+    plt.plot(sediment.x, sediment.species['OM']['concentration'][:, -1], 'b')
     plt.show()
