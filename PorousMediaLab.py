d2666778fe6cdddfcd7a79349892590d492eed4c
pycckuu <is.markelov@gmail.com>
2017-02-07 18:14:15 -0500

    FTC example plots

--- PorousMediaLab.py
+++ PorousMediaLab.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import numexpr as ne
 import time
 import sys
+import seaborn as sns
+from matplotlib.colors import ListedColormap
+sns.set_style("whitegrid")
 
 
 def ode_integrate(C0, dcdt, rates, coef, dt, solver=1):
@@ -337,9 +340,9 @@ class PorousMediaLab:
             plt.xlabel('Years, [year]')
         for depth in depths:
             lbl = str(depth) + ' cm'
-            plt.plot(t, self.species[element]['theta'] * self.species[element]['concentration'][int(depth / self.dx)][-num_of_elem:], label=lbl)
+            plt.plot(t, self.species[element]['theta'] * self.species[element]['concentration'][int(depth / self.dx)][-num_of_elem:], lw=3, label=lbl)
         ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-        ax.grid(linestyle='-', linewidth=0.1)
+        ax.grid(linestyle='-', linewidth=0.2)
         plt.show()
 
     def plot_times(self, element, time_slices=[0, 1, 2, 3, 4]):
@@ -354,9 +357,9 @@ class PorousMediaLab:
         plt.ylabel('Depth, cm')
         for tms in time_slices:
             lbl = '%.2f day' % (tms * 365)
-            plt.plot(self.species[element]['theta'] * self.species[element]['concentration'][:, int(tms / self.dt)], -self.x, label=lbl)
+            plt.plot(self.species[element]['theta'] * self.species[element]['concentration'][:, int(tms / self.dt)], -self.x, lw=3, label=lbl)
         ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol=2)
-        ax.grid(linestyle='-', linewidth=0.1)
+        ax.grid(linestyle='-', linewidth=0.2)
         plt.show()
 
     def plot_profiles(self):
@@ -365,16 +368,17 @@ class PorousMediaLab:
 
     def plot_profile(self, element):
         plt.figure()
-        plt.plot(self.species[element]['theta'] * self.profiles[element], -self.x, label=element)
+        plt.plot(self.species[element]['theta'] * self.profiles[element], -self.x, sns.xkcd_rgb["denim blue"], lw=3, label=element)
         if element == 'Temperature':
-            plt.title('Temperature profile')
+            plt.title('Temperature profile after %.2f days' % (self.time[-1] * 365))
             plt.xlabel('Temperature, C')
         else:
-            plt.title('Bulk ' + element + ' concentration')
+            plt.title('Bulk %s concentration after %.2f days' % (element, self.time[-1] * 365))
             plt.xlabel('mmol/L')
         plt.ylabel('Depth, cm')
         ax = plt.gca()
         ax.ticklabel_format(useOffset=False)
+        ax.grid(linestyle='-', linewidth=0.2)
         plt.legend()
         plt.tight_layout()
         plt.show()
@@ -392,7 +396,7 @@ class PorousMediaLab:
         else:
             X, Y = np.meshgrid(self.time[1::100], -self.x)
             plt.xlabel('Years, [year]')
-        CS = plt.contourf(X, Y, self.species[element]['theta'] * self.species[element]['concentration'][:, 0:-1:100], 51, origin='lower')
+        CS = plt.contourf(X, Y, self.species[element]['theta'] * self.species[element]['concentration'][:, 0:-1:100], 51, cmap=ListedColormap(sns.color_palette("Blues", 51)), origin='lower')
         if labels:
             plt.clabel(CS, inline=1, fontsize=10, colors='w')
         cbar = plt.colorbar(CS)
@@ -423,7 +427,7 @@ class PorousMediaLab:
         else:
             X, Y = np.meshgrid(self.time[1::100], -self.x)
             plt.xlabel('Years, [year]')
-        CS = plt.contourf(X, Y, z, 20, cmap=plt.cm.bwr, origin='lower', levels=lim, extend='both')
+        CS = plt.contourf(X, Y, z, 20, cmap=ListedColormap(sns.color_palette("RdBu_r", 101)), origin='lower', levels=lim, extend='both')
         if labels:
             plt.clabel(CS, inline=1, fontsize=10, colors='w')
         cbar = plt.colorbar(CS)
