ff269b09f693ca8f1d5ce84efd5d39c1bf3fd519
pycckuu <is.markelov@gmail.com>
2017-05-31 16:52:46 -0400

    refactored imported methods

--- src/PorousMediaLab.py
+++ src/PorousMediaLab.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy.sparse import spdiags
 import time
 import sys
-from scipy import special
 from joblib import Parallel, delayed
 import Ploter
 
@@ -39,30 +38,6 @@ class PorousMediaLab:
         self.constants['phi'] = self.phi
         self.num_adjustments = 0
         self.parallel = Parallel(n_jobs=4)
-
-    def plot_depths(self, **kwargs):
-        Ploter.plot_depths(self, **kwargs)
-
-    def plot_times(self, **kwargs):
-        Ploter.plot_times(self, **kwargs)
-
-    def plot_profiles(self, **kwargs):
-        Ploter.plot_profiles(self, **kwargs)
-
-    def plot_profile(self, **kwargs):
-        Ploter.plot_profile(self, **kwargs)
-
-    def plot_contourplots(self, **kwargs):
-        Ploter.plot_contourplots(self, **kwargs)
-
-    def contour_plot(self, **kwargs):
-        Ploter.contour_plot(self, **kwargs)
-
-    def plot_contourplots_of_rates(self, **kwargs):
-        Ploter.plot_contourplots_of_rates(self, **kwargs)
-
-    def contour_plot_of_rates(self, **kwargs):
-        Ploter.contour_plot_of_rates(self, **kwargs)
 
     def __getattr__(self, attr):
         return self.species[attr]
@@ -373,60 +348,14 @@ class PorousMediaLab:
     def is_solute(self, element):
         return self.species[element]['is_solute']
 
+    """Mapping of ploting methods from Ploter module"""
 
-def transport_equation_plot():
-    '''Check the transport equation integrator'''
-    w = 5
-    tend = 5
-    dx = 0.1
-    length = 100
-    phi = 1
-    dt = 0.001
-    lab = PorousMediaLab(length, dx, tend, dt, phi, w)
-    D = 5
-    lab.add_solute_species('O2', D, 0.0, 1)
-    lab.solve()
-    x = np.linspace(0, lab.length, lab.length / lab.dx + 1)
-    sol = 1 / 2 * (special.erfc((x - lab.w * lab.tend) / 2 / np.sqrt(D * lab.tend)) +
-                   np.exp(lab.w * x / D) * special.erfc((x + lab.w * lab.tend) / 2 / np.sqrt(D * lab.tend)))
-
-    plt.figure()
-    plt.plot(x, sol, 'k', label='Analytical solution')
-    plt.scatter(lab.x[::10], lab.species['O2'].concentration[
-                :, -1][::10], marker='x', label='Numerical')
-    plt.xlim([x[0], x[-1]])
-    ax = plt.gca()
-    ax.ticklabel_format(useOffset=False)
-    ax.grid(linestyle='-', linewidth=0.2)
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-
-
-def reaction_equation_plot():
-    '''Check the reaction equation integrator'''
-    C0 = {'C': 1}
-    coef = {'k': 2}
-    rates = {'R': 'k*C'}
-    dcdt = {'C': '-R'}
-    dt = 0.001
-    T = 10
-    time = np.linspace(0, T, T / dt + 1)
-    num_sol = np.array(C0['C'])
-    for i in range(1, len(time)):
-        C_new, _ = OdeSolver.ode_integrate(
-            C0, dcdt, rates, coef, dt, solver='rk4')
-        C0['C'] = C_new['C']
-        num_sol = np.append(num_sol, C_new['C'])
-    assert max(num_sol - np.exp(-coef['k'] * time)) < 1e-5
-
-    plt.figure()
-    plt.plot(time, np.exp(-coef['k'] * time), 'k', label='Analytical solution')
-    plt.scatter(time[::100], num_sol[::100], marker='x', label='Numerical')
-    plt.xlim([time[0], time[-1]])
-    ax = plt.gca()
-    ax.ticklabel_format(useOffset=False)
-    ax.grid(linestyle='-', linewidth=0.2)
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
+    custom_plot = Ploter.custom_plot
+    plot_depths = Ploter.plot_depths
+    plot_times = Ploter.plot_times
+    plot_profiles = Ploter.plot_profiles
+    plot_profile = Ploter.plot_profile
+    plot_contourplots = Ploter.plot_contourplots
+    contour_plot = Ploter.contour_plot
+    plot_contourplots_of_rates = Ploter.plot_contourplots_of_rates
+    contour_plot_of_rates = Ploter.contour_plot_of_rates
