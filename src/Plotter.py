57107b8911b34944580cd76d965ed0e1885e404d
pycckuu <is.markelov@gmail.com>
2017-07-11 16:24:54 -0400

    removed units from plots

--- src/Plotter.py
+++ src/Plotter.py
@@ -17,10 +17,10 @@ def custom_plot(lab, x, y, ttl='', y_lbl='', x_lbl=''):
     plt.ylabel(y_lbl)
     plt.xlabel(x_lbl)
     ax.grid(linestyle='-', linewidth=0.2)
-    plt.show()
+    return ax
 
 
-def plot_depths(lab, element, depths=[0, 1, 2, 3, 4], years_to_plot=10, days=True):
+def plot_depths(lab, element, depths=[0, 1, 2, 3, 4], time_to_plot=False):
     plt.figure()
     ax = plt.subplot(111)
     if element == 'Temperature':
@@ -28,24 +28,19 @@ def plot_depths(lab, element, depths=[0, 1, 2, 3, 4], years_to_plot=10, days=Tru
         plt.ylabel('Temperature, C')
     else:
         plt.title(element + ' concentration at specific depths')
-        plt.ylabel('mmol/L')
-    if lab.tend > years_to_plot:
-        num_of_elem = int(years_to_plot / lab.dt)
+        plt.ylabel('Concentration')
+    if time_to_plot:
+        num_of_elem = int(time_to_plot / lab.dt)
     else:
         num_of_elem = len(lab.time)
-    if days:
-        t = lab.time[-num_of_elem:] * 365
-        plt.xlabel('Days, [day]')
-    else:
-        t = lab.time[-num_of_elem:]
-        plt.xlabel('Years, [year]')
+    t = lab.time[-num_of_elem:]
+    plt.xlabel('Time')
     for depth in depths:
-        lbl = str(depth) + ' cm'
-        plt.plot(t, lab.species[element]['concentration'][
-                 int(depth / lab.dx)][-num_of_elem:], lw=3, label=lbl)
+        lbl = str(depth)
+        plt.plot(t, lab.species[element]['concentration'][int(depth / lab.dx)][-num_of_elem:], lw=3, label=lbl)
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     ax.grid(linestyle='-', linewidth=0.2)
-    plt.show()
+    return ax
 
 
 def plot_times(lab, element, time_slices=[0, 1, 2, 3, 4]):
@@ -56,15 +51,15 @@ def plot_times(lab, element, time_slices=[0, 1, 2, 3, 4]):
         plt.xlabel('Temperature, C')
     else:
         plt.title(element + ' concentration')
-        plt.xlabel('mmol/L')
+        plt.xlabel('Concentration')
     plt.ylabel('Depth, cm')
     for tms in time_slices:
-        lbl = '%.2f day' % (tms * 365)
+        lbl = 'at time: %.2f ' % (tms)
         plt.plot(lab.species[element]['concentration'][
                  :, int(tms / lab.dt)], -lab.x, lw=3, label=lbl)
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol=2)
     ax.grid(linestyle='-', linewidth=0.2)
-    plt.show()
+    return ax
 
 
 def plot_profiles(lab):
@@ -74,21 +69,20 @@ def plot_profiles(lab):
 
 def plot_profile(lab, element):
     plt.figure()
-    plt.plot(lab.profiles[element], -lab.x,
-             sns.xkcd_rgb["denim blue"], lw=3, label=element)
+    plt.plot(lab.profiles[element], -lab.x, sns.xkcd_rgb["denim blue"], lw=3, label=element)
     if element == 'Temperature':
-        plt.title('Temperature profile after %.2f days')
+        plt.title('Temperature profile after %.2f units of time')
         plt.xlabel('Temperature, C')
     else:
         plt.title('%s concentration' % (element, ))
-        plt.xlabel('mmol/L')
-    plt.ylabel('Depth, cm')
+        plt.xlabel('Concentration')
+    plt.ylabel('Depth')
     ax = plt.gca()
     ax.ticklabel_format(useOffset=False)
     ax.grid(linestyle='-', linewidth=0.2)
     plt.legend()
     plt.tight_layout()
-    plt.show()
+    return ax
 
 
 def plot_contourplots(lab, **kwargs):
@@ -107,7 +101,7 @@ def contour_plot(lab, element, labels=False, days=False, last_year=False):
         k = 1
     if days:
         X, Y = np.meshgrid(lab.time[k::n] * 365, -lab.x)
-        plt.xlabel('Days, [day]')
+        plt.xlabel('Time')
     else:
         X, Y = np.meshgrid(lab.time[k::n], -lab.x)
         plt.xlabel('Time')
@@ -117,14 +111,14 @@ def contour_plot(lab, element, labels=False, days=False, last_year=False):
     if labels:
         plt.clabel(CS, inline=1, fontsize=10, colors='w')
     cbar = plt.colorbar(CS)
-    plt.ylabel('Depth, [cm]')
+    plt.ylabel('Depth')
     ax = plt.gca()
     ax.ticklabel_format(useOffset=False)
-    cbar.ax.set_ylabel('%s [mmol/L]' % element)
+    cbar.ax.set_ylabel('%s concentration' % element)
     if element == 'Temperature':
         plt.title('Temperature contour plot')
         cbar.ax.set_ylabel('Temperature, C')
-    plt.show()
+    return ax
 
 
 def plot_contourplots_of_rates(lab, **kwargs):
@@ -135,7 +129,7 @@ def plot_contourplots_of_rates(lab, **kwargs):
         contour_plot_of_rates(lab, element, **kwargs)
 
 
-def contour_plot_of_rates(lab, element, labels=False, days=True, last_year=False):
+def contour_plot_of_rates(lab, element, labels=False, last_year=False):
     plt.figure()
     plt.title('Rate of %s consumption/production' % element)
     resoluion = 100
@@ -147,19 +141,15 @@ def contour_plot_of_rates(lab, element, labels=False, days=True, last_year=False
     z = lab.species[element]['rates'][:, k - 1:-1:n]
     lim = np.max(np.abs(z))
     lim = np.linspace(-lim - 0.1, +lim + 0.1, 51)
-    if days:
-        X, Y = np.meshgrid(lab.time[k::n] * 365, -lab.x)
-        plt.xlabel('Days, [day]')
-    else:
-        X, Y = np.meshgrid(lab.time[k::n], -lab.x)
-        plt.xlabel('Years, [year]')
+    X, Y = np.meshgrid(lab.time[k::n], -lab.x)
+    plt.xlabel('Time')
     CS = plt.contourf(X, Y, z, 20, cmap=ListedColormap(sns.color_palette(
         "RdBu_r", 101)), origin='lower', levels=lim, extend='both')
     if labels:
         plt.clabel(CS, inline=1, fontsize=10, colors='w')
     cbar = plt.colorbar(CS)
-    plt.ylabel('Depth, [cm]')
+    plt.ylabel('Depth')
     ax = plt.gca()
     ax.ticklabel_format(useOffset=False)
-    cbar.ax.set_ylabel('Rate %s [mmol/L/yr]' % element)
-    plt.show()
+    cbar.ax.set_ylabel('Rate %s [Concentration/Time]' % element)
+    return ax
