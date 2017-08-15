422300252b8c549466feba62d2f28b8f51ad97e2
pycckuu <is.markelov@gmail.com>
2017-08-14 20:44:07 -0400

    SPDIAGS: partially fixed bug with non-constant porosity

--- src/PorousMediaLab.py
+++ src/PorousMediaLab.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.sparse import spdiags
 import time
 import sys
+import traceback
 import Plotter
 
 import OdeSolver
@@ -84,7 +85,7 @@ class PorousMediaLab:
         self.species[element]['concentration'][:, 0] = self.species[element]['init_C']
         self.profiles[element] = self.species[element]['concentration'][:, 0]
         if rising_velocity:
-            self.species[element]['w'] = rising_velocity
+            self.species[element]['w'] = self.w + rising_velocity
         else:
             self.species[element]['w'] = self.w
         self.species[element]['int_transport'] = int_transport
@@ -121,6 +122,7 @@ class PorousMediaLab:
         self.update_matrices_due_to_bc(element, i)
 
     def template_AL_AR(self, element):
+        # TODO: error source somewhere in non constant porosity profile. Maybe we also need d phi/dx
         s = self.species[element]['theta'] * self.species[element]['D'] * self.dt / self.dx / self.dx
         q = self.species[element]['theta'] * self.species[element]['w'] * self.dt / self.dx
         self.species[element]['AL'] = spdiags(((-s / 2 + q / 4), (self.species[element]['theta'] + s), (-s / 2 - q / 4)), [-1, 0, 1], self.N, self.N, format='csc')  # .toarray()
@@ -133,9 +135,9 @@ class PorousMediaLab:
             self.species[element]['AR'][0, 1] = 0
         elif self.species[element]['bc_top_type'] in ['neumann', 'flux']:
             self.species[element]['AL'][0, 0] = self.species[element]['theta'][0] + s[0]
-            self.species[element]['AL'][0, 1] = -s[0]
+            self.species[element]['AL'][0, 1] = -s[1]
             self.species[element]['AR'][0, 0] = self.species[element]['theta'][0] - s[0]
-            self.species[element]['AR'][0, 1] = s[0]
+            self.species[element]['AR'][0, 1] = s[1]
         else:
             print('\nABORT!!!: Not correct top boundary condition type...')
             sys.exit()
@@ -147,9 +149,9 @@ class PorousMediaLab:
             self.species[element]['AR'][-1, -2] = 0
         elif self.species[element]['bc_bot_type'] in ['neumann', 'flux']:
             self.species[element]['AL'][-1, -1] = self.species[element]['theta'][-1] + s[-1]
-            self.species[element]['AL'][-1, -2] = -s[-1]
+            self.species[element]['AL'][-1, -2] = -s[-2]
             self.species[element]['AR'][-1, -1] = self.species[element]['theta'][-1] - s[-1]
-            self.species[element]['AR'][-1, -2] = s[-1]
+            self.species[element]['AR'][-1, -2] = s[-2]
         else:
             print('\nABORT!!!: Not correct bottom boundary condition type...')
             sys.exit()
@@ -186,7 +188,7 @@ class PorousMediaLab:
             self.species[element]['B'][-1] = self.species[element]['B'][-1] + 2 * self.species[element]['bc_bot'] * \
                 (2 * s[-1] - q[-1]) * self.dx / self.species[element]['theta'][-1] / self.species[element]['D']
         else:
-            print('\nABORT!!!: Not correct boundary condition...')
+            print('\nABORT!!!: Not correct boundary condition in the species...')
             sys.exit()
 
     def add_henry_law_equilibrium(self, aq, gas, Hcc):
@@ -199,35 +201,44 @@ class PorousMediaLab:
         """
         self.henry_law_equations.append({'aq': aq, 'gas': gas, 'Hcc': Hcc})
 
-    def add_acid_base_equilibrium(self, species, pKa):
-        acid = Acid(pKa=pKa, charge=0, conc=np.nan)
-        self.acid_base_components.append({'species': species, 'component': acid})
+    def add_ion(self, element, charge):
+        ion = Neutral(charge=charge, conc=np.nan)
+        self.acid_base_components.append({'species': [element], 'pH_object': ion})
 
-    def acid_base_equilibrium_integrate(self, i):
+    def add_acid(self, species, pKa, charge=0):
+        acid = Acid(pKa=pKa, charge=charge, conc=np.nan)
+        self.acid_base_components.append({'species': species, 'pH_object': acid})
 
+    def acid_base_equilibrium_solve(self, i):
+        self.acid_base_estimate_ph(i)
+        self.acid_base_update_conc_of_components(i)
+
+    def acid_base_update_conc_of_components(self, i):
+        for c in self.acid_base_components:
+            init_C = 0
+            alphas = c['pH_object'].alpha(self.species['pH']['concentration'][:, i])
+            for idx in range(len(c['species'])):
+                init_C += self.species[c['species'][idx]]['concentration'][:, i]
+            for idx in range(len(c['species'])):
+                self.species[c['species'][idx]]['concentration'][:, i] = init_C * alphas[:, idx]
+
+    def acid_base_estimate_ph(self, i):
+        res = self.species['pH']['concentration'][0, i - 1]  # initial guess from previous time-step
         for idx_j in range(self.N):
             for c in self.acid_base_components:
                 init_conc = 0
                 for element in c['species']:
                     init_conc += self.species[element]['concentration'][idx_j, i]
-                c['component'].conc = init_conc
+                c['pH_object'].conc = init_conc
             if idx_j == 0:
-                self.acid_base_system.pHsolve(guess=7, tol=1e-5)
+                self.acid_base_system.pHsolve(guess=7, tol=1e-4)
                 res = self.acid_base_system.pH
             else:
-                phs = np.linspace(res - 0.5, res + 0.5, 201)
+                phs = np.linspace(res - 0.1, res + 0.1, 201)
                 idx = self.acid_base_system._diff_pos_neg(phs).argmin()
                 res = phs[idx]
             self.species['pH']['concentration'][idx_j, i] = res
             self.profiles['pH'][idx_j] = res
-
-        for c in self.acid_base_components:
-            init_C = 0
-            alphas = c['component'].alpha(self.species['pH']['concentration'][:, i])
-            for idx in range(len(c['species'])):
-                init_C += self.species[c['species'][idx]]['concentration'][:, i]
-            for idx in range(len(c['species'])):
-                self.species[c['species'][idx]]['concentration'][:, i] = init_C * alphas[:, idx]
 
     def estimate_time_of_computation(self, i):
         if i == 1:
@@ -244,14 +255,13 @@ class PorousMediaLab:
     def create_acid_base_system(self):
         self.add_species(is_solute=True, element='pH', D=0, init_C=7, bc_top=7, bc_top_type='dirichlet',
                          bc_bot=7, bc_bot_type='dirichlet', rising_velocity=False, int_transport=False)
-        self.acid_base_system = System(*[c['component'] for c in self.acid_base_components])
+        self.acid_base_system = System(*[c['pH_object'] for c in self.acid_base_components])
 
     def pre_run_methods(self):
         if len(self.acid_base_components) > 0:
             self.create_acid_base_system()
 
     def solve(self, do_adjust=True):
-        self.pre_run_methods()
         print('Simulation starts  with following params:\n\ttend = %.1f,\n\tdt = %.2e,\n\tL = %.1f,\n\tdx = %.2e,\n\tw = %.2f' %
               (self.time[-1], self.dt, self.length, self.dx, self.w))
         with np.errstate(invalid='raise'):
@@ -261,6 +271,7 @@ class PorousMediaLab:
                     self.estimate_time_of_computation(i)
                 except FloatingPointError as inst:
                     print('\nABORT!!!: Numerical instability... Please, adjust dt and dx manually...')
+                    traceback.print_exc()
                     sys.exit()
 
     def integrate_one_timestep(self, i):
@@ -271,7 +282,7 @@ class PorousMediaLab:
         if self.henry_law_equations:
             self.henry_equilibrium_integrate(i)
         if self.acid_base_components:
-            self.acid_base_equilibrium_integrate(i)
+            self.acid_base_equilibrium_solve(i)
 
     def henry_equilibrium_integrate(self, i):
         for eq in self.henry_law_equations:
@@ -289,6 +300,8 @@ class PorousMediaLab:
         for element in C_new:
             if element is not 'Temperature':
                 # the concentration should be positive
+                # print(element)
+                # print(C_new[element])
                 C_new[element][C_new[element] < 0] = 0
             self.profiles[element] = C_new[element]
             self.species[element]['concentration'][:, i] = self.profiles[element]
