bc1a2d4ea3a3a2f24f3e929e790c827c58ca6f2a
pycckuu <is.markelov@gmail.com>
2019-09-06 16:23:49 +0200

    saving results in HDF5

---
+++ porousmedialab/saver.py
@@ -0,0 +1,66 @@
+import numpy as np
+import h5py
+import os
+
+
+
+def save_dict_to_hdf5(dic, filename):
+    """
+    Save a dictionary whose contents are only strings, np.float64, np.int64,
+    np.ndarray, and other dictionaries following this structure
+    to an HDF5 file. These are the sorts of dictionaries that are meant
+    to be produced by the ReportInterface__to_dict__() method.
+    """
+    with h5py.File(filename, 'w') as h5file:
+        recursively_save_dict_contents_to_group(h5file, '/', dic)
+
+def recursively_save_dict_contents_to_group(h5file, path, dic):
+    """
+    Take an already open HDF5 file and insert the contents of a dictionary
+    at the current path location. Can call itself recursively to fill
+    out HDF5 files with the contents of a dictionary.
+    """
+    for key, item in dic.items():
+        if isinstance(item, (np.ndarray, np.int64, np.float64, str, bytes)):
+            h5file[path + key] = item
+        elif isinstance(item, dict):
+            recursively_save_dict_contents_to_group(h5file, path + key + '/', item)
+        else:
+            raise ValueError('Cannot save %s type'%type(item))
+
+def load_dict_from_hdf5(filename):
+    """
+    Load a dictionary whose contents are only strings, floats, ints,
+    numpy arrays, and other dictionaries following this structure
+    from an HDF5 file. These dictionaries can then be used to reconstruct
+    ReportInterface subclass instances using the
+    ReportInterface.__from_dict__() method.
+    """
+    with h5py.File(filename, 'r') as h5file:
+        return recursively_load_dict_contents_from_group(h5file, '/')
+
+def recursively_load_dict_contents_from_group(h5file, path):
+    """
+    Load contents of an HDF5 group. If further groups are encountered,
+    treat them like dicts and continue to load them recursively.
+    """
+    ans = {}
+    for key, item in h5file[path].items():
+        if isinstance(item, h5py._hl.dataset.Dataset):
+            ans[key] = item.value
+        elif isinstance(item, h5py._hl.group.Group):
+            ans[key] = recursively_load_dict_contents_from_group(h5file, path + key + '/')
+    return ans
+
+if __name__ == '__main__':
+
+    data = {'x': 'astring',
+            'y': np.arange(10),
+            'd': {'z': np.ones((2,3)),
+                  'b': b'bytestring'}}
+    print(data)
+    filename = 'test.h5'
+    save_dict_to_hdf5(data, filename)
+    dd = load_dict_from_hdf5(filename)
+    print(dd)
+    # should test for bad type
